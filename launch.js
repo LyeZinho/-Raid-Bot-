587b2408f78e334e0c633785d0112de818e55c2c
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-11-24 14:45:52 +0000

    Initial commit

---
+++ launch.js
@@ -0,0 +1,10 @@
+const title = String.raw`
+                                                                                                 A
+`;
+
+console.log(title)
+
+console.log(``);
+console.log(`                                          Made by 7teen#1646`)
+console.log(``);
+console.log(``);
